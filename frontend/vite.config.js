dbf1d14bb671ef0b443d2a29875512fd55679f33
tunhihp <125249969+tunhihp@users.noreply.github.com>
2025-12-10 14:04:49 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 ﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-    },
-})
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://unaffected-valorie-unmovingly.ngrok-free.dev",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+});
